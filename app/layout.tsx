07b30bc5e09c0608f5124dd3668c021cb1c9041d
mahima-levein <mahima.levein@gmail.com>
2026-09-22 16:16:50 +0530

    Clarity Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SiteHeader } from "@/app/components/SiteHeader";
@@ -117,6 +118,15 @@ export default function RootLayout({
           <PageTransition>{children}</PageTransition>
         </main>
         <SiteFooter />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ym90llpm5k");
+          `}
+        </Script>
         <Analytics />
       </body>
     </html>
